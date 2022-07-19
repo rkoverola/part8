224bbfa2fbd6703a4c1af90c32422804372fd9e7
Risto Koverola <risto.koverola@eficode.com>
2022-07-19 16:54:40 +0300

    Save progress until 8.17

--- sandbox-front/src/App.js
+++ sandbox-front/src/App.js
@@ -1,10 +1,11 @@
-import { useQuery } from "@apollo/client";
+import { useApolloClient, useQuery } from "@apollo/client";
 import { useState } from "react";
 
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
 import { ALL_PERSONS } from "./queries";
 import PhoneForm from "./components/PhoneForm";
+import LoginForm from "./components/LoginForm";
 
 const Notify = ({ errorMessage }) => {
   if (!errorMessage) {
@@ -16,10 +17,18 @@ const Notify = ({ errorMessage }) => {
 const App = () => {
   const result = useQuery(ALL_PERSONS);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [token, setToken] = useState(null);
+  const client = useApolloClient();
 
   if (result.loading) {
     return <div>Loading...</div>;
   }
+
+  const logout = () => {
+    setToken(null);
+    localStorage.clear();
+    client.resetStore();
+  };
 
   const notify = (message) => {
     setErrorMessage(message);
@@ -28,9 +37,20 @@ const App = () => {
     }, 10000);
   };
 
+  if (!token) {
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <h2>Login</h2>
+        <LoginForm setToken={setToken} setError={notify} />
+      </div>
+    );
+  }
+
   return (
     <div>
       <Notify errorMessage={errorMessage} />
+      <button onClick={logout}>Logout</button>
       <Persons persons={result.data.allPersons} />
       <PersonForm setError={notify} />
       <PhoneForm setError={notify} />
