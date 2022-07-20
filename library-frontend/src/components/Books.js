774e64642acee40f02a7e4f6acfb97c646cba35a
Risto Koverola <risto.koverola@eficode.com>
2022-07-20 14:15:42 +0300

    Solve 8.19

--- library-frontend/src/components/Books.js
+++ library-frontend/src/components/Books.js
@@ -1,8 +1,35 @@
+import { useState } from "react";
 import { useQuery } from "@apollo/client";
 
 import { ALL_BOOKS } from "../queries";
 
+const AllGenresButton = (props) => {
+  return (
+    <button
+      onClick={() => {
+        props.setGenreFilter(null);
+      }}
+    >
+      all genres
+    </button>
+  );
+};
+
+const GenreButton = (props) => {
+  return (
+    <button
+      onClick={() => {
+        props.setGenreFilter(props.name);
+      }}
+    >
+      {props.name}
+    </button>
+  );
+};
+
 const Books = (props) => {
+  const [genreFilter, setGenreFilter] = useState(null);
+
   const result = useQuery(ALL_BOOKS);
 
   if (!props.show || result.loading) {
@@ -10,11 +37,22 @@ const Books = (props) => {
   }
 
   const books = result.data.allBooks;
+  const filteredBooks = genreFilter
+    ? books.filter((b) => b.genres.includes(genreFilter))
+    : books;
+  const genres = result.data.allBooks.flatMap((b) => b.genres);
+  const uniqueGenres = [...new Set(genres)];
+  const currentFilterText = genreFilter
+    ? `showing: ${genreFilter}`
+    : "all genres";
   console.log("Got books", books);
+  console.log("Got genres", uniqueGenres);
 
   return (
     <div>
       <h2>books</h2>
+
+      <h3>{currentFilterText}</h3>
 
       <table>
         <tbody>
@@ -23,7 +61,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((b) => (
+          {filteredBooks.map((b) => (
             <tr key={b.title}>
               <td>{b.title}</td>
               <td>{b.author.name}</td>
@@ -32,6 +70,13 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
+      <h3>filter by genre</h3>
+      <div>
+        {uniqueGenres.map((g) => (
+          <GenreButton key={g} name={g} setGenreFilter={setGenreFilter} />
+        ))}
+        <AllGenresButton setGenreFilter={setGenreFilter} />
+      </div>
     </div>
   );
 };
